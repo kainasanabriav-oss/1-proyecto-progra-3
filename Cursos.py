004b7ce6c85a994fb9c5579c693e7b5bb5e5f94f
SirDior777 <cristesteban09@gmail.com>
2026-09-24 14:08:28 -0600

    cursos

--- Cursos.py
+++ Cursos.py
@@ -1,1 +1,14 @@
-#Cursos
+class Curso:
+
+    def __init__(self,referencia,descripcion,fechaInicio,fechaFinal,estado,docente=None,horasCapacitacion=None,horasAsesoria=None,mes=None
+    ):
+        self.referencia = referencia
+        self.descripcion = descripcion
+        self.fechaInicio = fechaInicio
+        self.fechaFinal = fechaFinal
+        self.estado = estado
+        self.docente = docente
+        self.horasCapacitacion = horasCapacitacion
+        self.horasAsesoria = horasAsesoria
+        self.mes = mes
+
